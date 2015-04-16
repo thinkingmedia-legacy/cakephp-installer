3bbb127fd2f13e47ca3e54d8ef7e0d1b983abc70
thinkingmedia <mathew@thinkingmedia.ca>
2015-04-16 18:23:22 -0400

    updated.

--- src/Hash.php
+++ src/Hash.php
@@ -5,7 +5,8 @@ namespace Installer;
 /**
  * @readme Usage.Hash Hash
  *
- * The `Hash` class uses the [mcrypt_create_iv](http://php.net/manual/en/function.mcrypt-create-iv.php) to generate passwords and salt values.
+ * The `Hash` class uses the [mcrypt_create_iv](http://php.net/manual/en/function.mcrypt-create-iv.php) to generate
+ *     passwords and salt values.
  *
  * ```PHP
  * \Installer\Hash::generate(15);
@@ -26,6 +27,7 @@ class Hash
      */
     public static function salt()
     {
+        return '';
     }
 
     /**
@@ -33,5 +35,6 @@ class Hash
      */
     public static function mysql_password()
     {
+        return '';
     }
 }
